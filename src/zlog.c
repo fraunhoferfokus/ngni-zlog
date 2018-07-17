c02b1ee189e4ae299852dc52735ec58b06214b03
Manar Zaboub <mza@flint.fokus.fraunhofer.de>
2018-07-17 16:35:07 +0200

    Integrating with phoenix libraries

--- src/zlog.c
+++ src/zlog.c
@@ -20,9 +20,9 @@
 #include "zc_defs.h"
 #include "rule.h"
 #include "version.h"
-#include "../xml/xml_helper.h"
-#include "../../utils/sockets/tcp_comm.h"
-#include "../bin/bin.h"
+#include "../../../utils/xml/xml_helper.h"
+#include "../../../utils/sockets/tcp_comm.h"
+#include "../../../utils/bin/bin.h"
 
 #define TCP_REC_BUFFER 120
 
